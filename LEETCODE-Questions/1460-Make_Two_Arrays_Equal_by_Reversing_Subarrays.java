810cb90cba52bccb85cf5b61843003f95fe91e74
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-08-04 00:25:50 +0530

    Added 1460-Make_Two_Arrays_Equal_by_Reversing_Subarrays.java

--- LEETCODE-Questions/1460-Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
+++ LEETCODE-Questions/1460-Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
@@ -1,24 +1,44 @@
 class Solution {
-    public boolean canBeEqual(int[] target, int[] arr) {
-        int n = target.length;
+    // TC: O(2N), SC: O(N)
+    // public boolean canBeEqual(int[] target, int[] arr) {
+    //     int n = target.length;
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+    //     HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int num: target){
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
+    //     for(int num: target){
+    //         map.put(num, map.getOrDefault(num, 0) + 1);
+    //     }
 
-        // System.out.println(map);
+    //     for(int num: arr){
+    //         if(!map.containsKey(num)){
+    //             return false;
+    //         }
+
+    //         if(map.get(num) == 1){
+    //             map.remove(num);
+    //         }else{
+    //             map.put(num, map.get(num) - 1);
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+    // TC: O(2N) * O(1001), SC: O(1001)
+    public boolean canBeEqual(int[] target, int[] arr){
+        int[] freq = new int[1001];
 
         for(int num: arr){
-            if(!map.containsKey(num)){
-                return false;
-            }
+            freq[num]++;
+        }
 
-            if(map.get(num) == 1){
-                map.remove(num);
-            }else{
-                map.put(num, map.get(num) - 1);
+        for(int num: target){
+            freq[num]--;
+        }
+
+        for(int num: freq){
+            if(num != 0){
+                return false;
             }
         }
 
