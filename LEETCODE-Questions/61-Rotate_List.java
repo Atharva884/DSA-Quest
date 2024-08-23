149e54dbe47a588542723b17efc9f9f2e8cb88d5
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-08-23 18:41:58 +0530

    Added 61-Rotate_List.java

---
+++ LEETCODE-Questions/61-Rotate_List.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode findNthNode(ListNode head, int k){
+        int count = 1;
+        ListNode temp = head;
+
+        while(temp.next != null && count != k){
+            temp = temp.next;
+            count++;
+        }
+
+        return temp;
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null) return null;
+
+        ListNode tail = head;
+        int length = 1;
+
+        while(tail.next != null){
+            tail = tail.next;
+            length++;
+        }
+
+
+        if(k % length == 0) return head;
+
+        k = k % length;
+        tail.next =  head;
+
+        ListNode newLastNode = findNthNode(head, length-k);
+        head = newLastNode.next;
+        newLastNode.next = null;
+
+        return head;
+    }
+}
