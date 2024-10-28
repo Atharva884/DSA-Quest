0d6c5d82f0004ec25315d9a848d1933f6fdab01f
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-10-28 18:00:29 +0530

    Added 876-Middle_of_the_Linked_List.java

--- LEETCODE-Questions/876-Middle_of_the_Linked_List.java
+++ LEETCODE-Questions/876-Middle_of_the_Linked_List.java
@@ -39,15 +39,38 @@ class Solution {
     //     return temp;
     // }
 
+    // Way - 1
+    // public static ListNode middleNode(ListNode head){
+    //     ListNode temp = head;
+
+    //     ListNode slow = head;
+    //     ListNode fast = head;
+
+    //     while(fast != null && fast.next != null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
+
+    //     return slow;
+    // }
+
+    // Way - 2
+    // Needed for splitting the linked list in middle
+    // We can't actually split the linked list efficiently with way 1
     public static ListNode middleNode(ListNode head){
         ListNode temp = head;
 
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null){
+        while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
+        }
+
+        // If fast is not reached null, then we are processing the odd ll
+        if(fast.next != null){
+            return slow.next;
         }
 
         return slow;
