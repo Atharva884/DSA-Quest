68c35f50569be3ed65cebdf46d3d560162452a11
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-11-30 00:31:05 +0530

    Added 946-Validate_Stack_Sequences.java

--- LEETCODE-Questions/946-Validate_Stack_Sequences.java
+++ LEETCODE-Questions/946-Validate_Stack_Sequences.java
@@ -1,52 +1,16 @@
 class Solution {
-    // TC: O(N), SC: O(N)
-    // public boolean validateStackSequences(int[] pushed, int[] popped) {
-    //     int n = pushed.length;
-
-    //     Stack<Integer> st = new Stack<>();
-
-    //     int i = 0;
-    //     int j = 0;
-
-    //     while(i < n && j < n){
-    //         // If they are same, pop it and move pointer
-    //         if(pushed[i] == popped[j]){
-    //             i++;
-    //             j++;
-    //         }else if(!st.isEmpty() && st.peek() == popped[j]){
-    //             st.pop();
-    //             j++;
-    //         }else{
-    //             st.push(pushed[i]);
-    //             i++;
-    //         }
-    //     }
-
-    //     while(j < n){
-    //         if(!st.isEmpty() && st.peek() == popped[j]){
-    //             st.pop();
-    //         }
-    //         j++;
-    //     }
-
-    //     return st.isEmpty();
-    // }
-
-    // StraightForward Approach
-    // // TC: O(N), SC: O(N)
-    public boolean validateStackSequences(int[] pushed, int[] popped){
-        Stack<Integer> st = new Stack<>();
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
         int n = pushed.length;
+
+        Stack<Integer> st = new Stack<>();
+
         int i = 0;
         int j = 0;
 
-
         while(i < n){
-            // Try inserting into the stack first and popped it if they are equal
             st.push(pushed[i]);
             i++;
 
-            // Popped while they are equal
             while(!st.isEmpty() && st.peek() == popped[j]){
                 st.pop();
                 j++;
