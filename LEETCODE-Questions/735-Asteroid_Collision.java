a850837800df37ae7bacae2115e997984e277c48
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-11-30 00:29:02 +0530

    Added 735-Asteroid_Collision.java

--- LEETCODE-Questions/735-Asteroid_Collision.java
+++ LEETCODE-Questions/735-Asteroid_Collision.java
@@ -2,37 +2,36 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         int n = asteroids.length;
         Stack<Integer> st = new Stack<>();
-      
+
         for(int i=0; i<n; i++){
             int asteroid = asteroids[i];
 
-            // If the asteroid is moving to left
+            if(st.isEmpty()){
+                st.push(asteroid);
+                continue;
+            }
+
             if(asteroid < 0){
-                while(!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(asteroid)){
+                while(!st.isEmpty() && st.peek() > 0 && st.peek() < -asteroid){
                     st.pop();
                 }
 
-                // Check for same size asteroid
-                if(!st.isEmpty() && st.peek() == Math.abs(asteroid)){
+                if(!st.isEmpty() && st.peek() == -asteroid){
                     st.pop();
-                }else if(st.isEmpty() || st.peek() < 0){ 
-                    // Insert, if st is empty i.e there are no asteroid moving in right and if the
-                    // top element on the stack is less than 0 means the asteroid is moving in left dir
+                }else if(st.isEmpty() || st.peek() < 0){
                     st.push(asteroid);
                 }
             }else{
-                // If the asteroid is moving to right
                 st.push(asteroid);
             }
-        }
-         
-        int[] arr = new int[st.size()];
-        int idx = st.size() - 1;
 
-        while(!st.isEmpty()){
-            arr[idx--] = st.pop();
         }
-        
-        return arr;
+
+        int[] ans = new int[st.size()];
+        for(int i=ans.length - 1; i>=0; i--){
+            ans[i] = st.pop();
+        }
+       
+        return ans;
     }
 }
