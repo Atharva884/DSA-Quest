46a8aa649bfcfe588930767bd45284f30ac171b8
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-11-30 22:39:28 +0530

    Added Top_View_of_Binary_Tree.java

---
+++ GFG-DAILY/Top_View_of_Binary_Tree.java
@@ -0,0 +1,179 @@
+// User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+class Pair{
+    Node node;
+    int level;
+    
+    public Pair(Node _node, int _level){
+        this.node = _node;
+        this.level = _level;
+    }
+}
+
+class Solution {
+    
+    static ArrayList<Integer> topView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        if(root == null){
+            return ans;
+        }
+        
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root, 0));
+        
+        while(q.size() != 0){
+            Pair pair = q.poll();
+            Node rem = pair.node;
+            int level = pair.level;
+            
+            if(!map.containsKey(level)){
+                map.put(level, new ArrayList<>());
+            }
+            map.get(level).add(rem.data);
+            
+            if(rem.left != null){
+                q.add(new Pair(rem.left, level - 1));
+            }
+            
+            if(rem.right != null){
+                q.add(new Pair(rem.right, level + 1));
+            }
+            
+        }
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int key: map.keySet()){
+            min = Math.min(min, key);
+            max = Math.max(max, key);
+        }
+        
+        
+        for(int i=min; i<=max; i++){
+            List<Integer> tmp = map.get(i);
+            if(tmp.size() > 0){
+                ans.add(tmp.get(0));
+            }
+        }
+        
+        return ans;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Tree {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Solution ob = new Solution();
+
+            ArrayList<Integer> vec = ob.topView(root);
+            for (int x : vec) System.out.print(x + " ");
+            System.out.println();
+
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
