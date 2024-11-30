cd2ea703b62547409ca8680c1877d0d280d40689
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-11-30 22:04:31 +0530

    Added Left_View_of_Binary_Tree.java

--- GFG-DAILY/Left_View_of_Binary_Tree.java
+++ GFG-DAILY/Left_View_of_Binary_Tree.java
@@ -1,4 +1,4 @@
-//User function Template for Java
+// User function Template for Java
 
 /* A Binary Tree node
 class Node
@@ -12,39 +12,38 @@ class Node
         left = right = null;
     }
 }*/
-class Tree
-{
-    //Function to return list containing elements of left view of binary tree.
-    ArrayList<Integer> leftView(Node root)
-    {
-      ArrayList<Integer> arr = new ArrayList<>();
+class Solution {
+    // Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
         
-      if(root == null) return arr;
-      
-      Queue<Node> q = new LinkedList<>();
-      
-      q.add(root);
-      
-      while(q.size() > 0){
-          int size = q.size();
-          
-          for(int i=0; i<size; i++){
-              Node rem = q.poll();
-              
-              if(i == 0){
-                  arr.add(rem.data);
-              }
-              
-              if(rem.left != null){
-                  q.offer(rem.left);
-              }
-              
-              if(rem.right != null){
-                  q.offer(rem.right);
-              }
-          }
-      }
-      
-      return arr;
+        if(root == null){
+            return ans;
+        }
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        while(q.size() != 0){
+            int size = q.size();
+            
+            for(int i=0; i<size; i++){
+                Node rem = q.poll();
+                
+                if(i == 0){
+                    ans.add(rem.data);
+                }
+                
+                if(rem.left != null){
+                    q.add(rem.left);
+                }
+                
+                if(rem.right != null){
+                    q.add(rem.right);
+                }
+            }
+        }
+        
+        return ans;
     }
 }
