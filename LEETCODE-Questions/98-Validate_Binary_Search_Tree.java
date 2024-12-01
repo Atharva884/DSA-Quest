1105da1cb9a86993dd0fae24f4dc1645c970df88
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-12-01 18:18:27 +0530

    Added 98-Validate_Binary_Search_Tree.java

--- LEETCODE-Questions/98-Validate_Binary_Search_Tree.java
+++ LEETCODE-Questions/98-Validate_Binary_Search_Tree.java
@@ -14,24 +14,57 @@
  * }
  */
 class Solution {
-    public void solve(TreeNode root, ArrayList<Integer> arr){
-        if(root == null) return;
+    // Brute Force Approach
+    // TC: O(2N), SC: O(N)
 
-        solve(root.left, arr);
-        arr.add(root.val);
-        solve(root.right, arr);
+    // public void solve(TreeNode root, ArrayList<Integer> arr){
+    //     if(root == null) return;
+
+    //     solve(root.left, arr);
+    //     arr.add(root.val);
+    //     solve(root.right, arr);
+    // }
+
+    // public boolean isValidBST(TreeNode root) {
+    //     ArrayList<Integer> arr = new ArrayList<>();
+    //     solve(root, arr);
+
+    //     for(int i=0; i<arr.size()-1; i++){
+    //         if(arr.get(i+1) <= arr.get(i)){
+    //             return false;
+    //         }
+    //     }
+        
+    //     return true;
+    // }
+
+    // Optimal Approach
+    // TC: O(N), SC: O(H)
+    public boolean solve(TreeNode root, long min, long max){
+        if(root == null){
+            return true;
+        }
+
+        long val = root.val;
+
+        if(val < min || val > max){
+            return false;
+        }
+
+        boolean l = solve(root.left, min, val - 1);
+        boolean r = solve(root.right, val + 1, max);
+
+        if(l == false || r == false){
+            return false;
+        }
+
+        return true;
     }
 
-    public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        solve(root, arr);
+    public boolean isValidBST(TreeNode root){
+        long min = Long.MIN_VALUE;
+        long max = Long.MAX_VALUE;
 
-        for(int i=0; i<arr.size()-1; i++){
-            if(arr.get(i+1) <= arr.get(i)){
-                return false;
-            }
-        }
-        
-        return true;
+        return solve(root, min, max);
     }
 }
