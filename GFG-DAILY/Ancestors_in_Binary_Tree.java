827f13df21654e320df463128a7612335a9775d1
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-12-02 15:58:00 +0530

    Added Ancestors_in_Binary_Tree.java

--- GFG-DAILY/Ancestors_in_Binary_Tree.java
+++ GFG-DAILY/Ancestors_in_Binary_Tree.java
@@ -11,34 +11,66 @@ class Node
 }*/
 class Solution {
     
-    public void solve(Node root, ArrayList<Integer> ans, ArrayList<Integer> newArr, int target){
-        if(root == null){
-            return;
-        }
+    // Not Optimized
+    
+    // public void solve(Node root, ArrayList<Integer> ans, ArrayList<Integer> newArr, int target){
+    //     if(root == null){
+    //         return;
+    //     }
         
-        ans.add(root.data);
+    //     ans.add(root.data);
+        
+    //     if(root.data == target){
+    //         ans.remove(ans.size() - 1);
+    //         for(int i=ans.size()-1; i>=0; i--){
+    //             newArr.add(ans.get(i));
+    //         }
+    //         return;
+    //     }
+        
+    //     solve(root.left, ans, newArr, target);
+    //     solve(root.right, ans, newArr, target);
+        
+        
+    //     ans.remove(ans.size() - 1);
+    // }
+
+    // public ArrayList<Integer> Ancestors(Node root, int target) {
+    //     ArrayList<Integer> ans = new ArrayList<>();
+    //     ArrayList<Integer> newArr = new ArrayList<>();
+        
+    //     solve(root, ans, newArr, target);
+        
+    //     return newArr;
+    // }
+    
+    public boolean solve(Node root, ArrayList<Integer> ans, int target){
+        if(root == null){
+            return false;
+        }
         
         if(root.data == target){
-            ans.remove(ans.size() - 1);
-            for(int i=ans.size()-1; i>=0; i--){
-                newArr.add(ans.get(i));
-            }
-            return;
+            return true;
         }
         
-        solve(root.left, ans, newArr, target);
-        solve(root.right, ans, newArr, target);
+        boolean l = solve(root.left, ans, target);
+        boolean r = solve(root.right, ans, target);
         
+        if(l == true || r == true){
+            ans.add(root.data);
+            return true;
+        }
         
-        ans.remove(ans.size() - 1);
+        return false;
     }
-
+    
     public ArrayList<Integer> Ancestors(Node root, int target) {
         ArrayList<Integer> ans = new ArrayList<>();
-        ArrayList<Integer> newArr = new ArrayList<>();
         
-        solve(root, ans, newArr, target);
+        solve(root, ans, target);
         
-        return newArr;
+        return ans;
     }
+    
+    
 }
