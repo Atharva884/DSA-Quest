0f4a668a776765b5536bad9f2e9f0139370bc754
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-12-07 17:12:14 +0530

    Added Floyd_Warshall.java

---
+++ GFG-DAILY/Floyd_Warshall.java
@@ -0,0 +1,41 @@
+// User function template for JAVA
+
+class Solution {
+    public void shortestDistance(int[][] mat) {
+        int n = mat.length;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == -1){
+                    mat[i][j] = 10001;
+                }
+                
+                if(i == j){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        
+        
+        for(int via=0; via<n; via++){
+            
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if(mat[i][via] != 10001 && mat[i][via] != 0 && mat[via][j] != 10001 && mat[via][j] != 0){
+                        mat[i][j] = Math.min(mat[i][j], mat[i][via] + mat[via][j]);
+                    }
+                }
+            }
+            
+        }
+        
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == 10001){
+                    mat[i][j] = -1;
+                }
+            }
+        }
+    }
+}
