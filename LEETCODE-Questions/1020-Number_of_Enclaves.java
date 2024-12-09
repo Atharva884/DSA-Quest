59a27bc004d18922b515fa91b362fdfafcf6a123
Atharva Jadhav <atharvalolzzz96@gmail.com>
2024-12-10 01:18:55 +0530

    Added 1020-Number_of_Enclaves.java

--- LEETCODE-Questions/1020-Number_of_Enclaves.java
+++ LEETCODE-Questions/1020-Number_of_Enclaves.java
@@ -1,25 +1,25 @@
 class Solution {
-    public void dfs(int i, int j, int n, int m, int[][] grid, boolean[][] arr) {
-        arr[i][j] = true;
+    public void dfs(int i, int j, int n, int m, int[][] grid) {
+        grid[i][j] = 0;
 
         // Top Dir
-        if (i - 1 >= 0 && grid[i - 1][j] == 1 && !arr[i - 1][j]) {
-            dfs(i - 1, j, n, m, grid, arr);
+        if (i - 1 >= 0 && grid[i - 1][j] == 1) {
+            dfs(i - 1, j, n, m, grid);
         }
 
         // Right Dir
-        if (j + 1 < m && grid[i][j + 1] == 1 && !arr[i][j + 1]) {
-            dfs(i, j + 1, n, m, grid, arr);
+        if (j + 1 < m && grid[i][j + 1] == 1) {
+            dfs(i, j + 1, n, m, grid);
         }
 
         // Bottom Dir
-        if (i + 1 < n && grid[i + 1][j] == 1 && !arr[i + 1][j]) {
-            dfs(i + 1, j, n, m, grid, arr);
+        if (i + 1 < n && grid[i + 1][j] == 1) {
+            dfs(i + 1, j, n, m, grid);
         }
 
         // Left Dir
-        if (j - 1 >= 0 && grid[i][j - 1] == 1 && !arr[i][j - 1]) {
-            dfs(i, j - 1, n, m, grid, arr);
+        if (j - 1 >= 0 && grid[i][j - 1] == 1) {
+            dfs(i, j - 1, n, m, grid);
         }
     }
 
@@ -27,41 +27,31 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
-        boolean[][] arr = new boolean[n][m];
-
         // Check the first row for any 0's
         for (int i = 0; i < m; i++) {
             if (grid[0][i] == 1) {
-                dfs(0, i, n, m, grid, arr);
+                dfs(0, i, n, m, grid);
             }
         }
 
         // Check the first col for any 0's
         for (int i = 0; i < n; i++) {
             if (grid[i][0] == 1) {
-                dfs(i, 0, n, m, grid, arr);
+                dfs(i, 0, n, m, grid);
             }
         }
 
         // Check the last row for any 0's
         for (int i = 0; i < m; i++) {
             if (grid[n - 1][i] == 1) {
-                dfs(n - 1, i, n, m, grid, arr);
+                dfs(n - 1, i, n, m, grid);
             }
         }
 
         // Check the last col for any 0's
         for (int i = 0; i < n; i++) {
             if (grid[i][m - 1] == 1) {
-                dfs(i, m - 1, n, m, grid, arr);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] == true) {
-                    grid[i][j] = 0;
-                }
+                dfs(i, m - 1, n, m, grid);
             }
         }
 
