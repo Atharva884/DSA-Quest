832f3c820590fdddfe1db99d73f3b3527830c4aa
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-03 18:43:41 +0530

    Added 0_-_1_Knapsack_Problem.java

---
+++ GFG-DAILY/0_-_1_Knapsack_Problem.java
@@ -0,0 +1,32 @@
+class Solution {
+    static int solve(int i, int[] val, int[] weight, int k, int[][] dp){
+        if(i < 0){
+            return 0;
+        }
+
+        if(dp[i][k] != -1){
+            return dp[i][k];   
+        }
+    
+        int notPick = solve(i-1, val, weight, k, dp);
+        int pick = 0;
+        if(k >= weight[i]){
+            pick = val[i] + solve(i-1, val, weight, k - weight[i], dp);
+        }
+
+        return dp[i][k] = Math.max(pick, notPick);        
+    }
+    
+    static int knapSack(int capacity, int val[], int wt[]) {
+        int n = val.length;
+        int[][] dp = new int[n][capacity + 1];
+        
+        for(int[] arr: dp){
+            for(int i=0; i<arr.length; i++){
+                arr[i] = -1;
+            }
+        }
+        
+        return solve(n-1, val, wt, capacity, dp);
+    }
+}
