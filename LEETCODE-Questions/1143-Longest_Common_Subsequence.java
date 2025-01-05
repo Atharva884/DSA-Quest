b055ebd607db322c518466054b05c86e5f7c7a3b
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-05 18:07:59 +0530

    Added 1143-Longest_Common_Subsequence.java

--- LEETCODE-Questions/1143-Longest_Common_Subsequence.java
+++ LEETCODE-Questions/1143-Longest_Common_Subsequence.java
@@ -1,29 +1,29 @@
-import java.util.*;
-
 class Solution {
-    public int f(int s1, int s2, String text1, String text2, int[][] dp) {
-        if (s1 < 0 || s2 < 0)
-            return 0;
-        if (dp[s1][s2] != -1)
-            return dp[s1][s2];
+    public int solve(String s1, int i, String s2, int j, int[][] dp){
+        if(i < 0 || j < 0) return 0;
 
-        // If the chars are matching
-        if (text1.charAt(s1) == text2.charAt(s2)) {
-            return dp[s1][s2] = 1 + f(s1 - 1, s2 - 1, text1, text2, dp);
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] = 1 + solve(s1, i-1, s2, j-1, dp);
         }
 
-        // If they aren't matching
-        return dp[s1][s2] = Math.max(f(s1 - 1, s2, text1, text2, dp), f(s1, s2 - 1, text1, text2, dp));
+        int x = solve(s1, i-1, s2, j, dp);
+        int y = solve(s1, i, s2, j-1, dp);
+
+        return dp[i][j] = Math.max(x, y);
     }
 
+
     public int longestCommonSubsequence(String text1, String text2) {
-        int s1 = text1.length();
-        int s2 = text2.length();
-        int[][] dp = new int[s1][s2];
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n][m];
+        for(int[] arr: dp){
+            Arrays.fill(arr, -1);
         }
 
-        return f(s1 - 1, s2 - 1, text1, text2, dp);
+        return solve(text1, n-1, text2, m-1, dp);
     }
 }
