f823f14feab32cc5e04dc2467698c6d620a9f748
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-07 12:40:02 +0530

    Added 309-Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java

--- LEETCODE-Questions/309-Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
+++ LEETCODE-Questions/309-Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -1,5 +1,20 @@
 class Solution {
-    // 
+    // public int solve(int idx, int buy, int[] prices, int n, int[][] dp){
+    //     if(idx >= n) return 0;
+    //     if(dp[idx][buy] != -1) return dp[idx][buy];
+
+    //     if(buy == 1){
+    //         int take = -prices[idx] + solve(idx+1, 0, prices, n, dp);
+    //         int notTake = solve(idx+1, 1, prices, n, dp);
+
+    //         return dp[idx][buy] = Math.max(take, notTake);
+    //     }
+
+    //     int take = prices[idx] + solve(idx+2, 1, prices, n, dp);
+    //     int notTake = solve(idx+1, 0, prices, n, dp);
+
+    //     return dp[idx][buy] = Math.max(take, notTake);
+    // }
 
     // Recursion + Memoization
     // TC: O(N*2), SC: O(N*2) + stack space
