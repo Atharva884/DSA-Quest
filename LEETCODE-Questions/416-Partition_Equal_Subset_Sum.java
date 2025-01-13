51ac6142c74794d0e2ae0df35d0bc4274db772dc
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-13 16:51:58 +0530

    Added 416-Partition_Equal_Subset_Sum.java

--- LEETCODE-Questions/416-Partition_Equal_Subset_Sum.java
+++ LEETCODE-Questions/416-Partition_Equal_Subset_Sum.java
@@ -1,38 +1,80 @@
-public boolean canPartition(int[] nums) {
-    int sum = 0;
-    
-    for (int num : nums) {
-        sum += num;
-    }
-    
-    if ((sum & 1) == 1) {
-        return false;
-    }
-    sum /= 2;
+class Solution {
+    // public boolean solve(int i, int sm, int[] nums, int target, int[][] dp){
+    //     if(sm == target) return true;
 
-    int n = nums.length;
-    boolean[][] dp = new boolean[n+1][sum+1];
-    for (int i = 0; i < dp.length; i++) {
-        Arrays.fill(dp[i], false);
-    }
-    
-    dp[0][0] = true;
-    
-    for (int i = 1; i < n+1; i++) {
-        dp[i][0] = true;
-    }
-    for (int j = 1; j < sum+1; j++) {
-        dp[0][j] = false;
-    }
-    
-    for (int i = 1; i < n+1; i++) {
-        for (int j = 1; j < sum+1; j++) {
-            dp[i][j] = dp[i-1][j];
-            if (j >= nums[i-1]) {
-                dp[i][j] = (dp[i][j] || dp[i-1][j-nums[i-1]]);
+    //     if(i < 0) return false;
+
+    //     if(dp[i][sm] != -1) return dp[i][sm] == 1 ? true : false;
+
+    //     boolean x = solve(i-1, sm, nums, target, dp);
+    //     boolean y = false;
+
+    //     if(sm + nums[i] <= target){
+    //         y = solve(i-1, sm + nums[i], nums, target, dp);
+    //     } 
+
+    //     dp[i][sm] = (x || y) ? 1 : 0;
+    //     return (x || y);
+    // }
+
+    // Recursion + Memoization
+    // TC: O(N*k), SC: O(N*k) + with stack space: O(N + k) where (k is sum / 2)
+
+    // public boolean canPartition(int[] nums) {
+    //     int n = nums.length;
+
+    //     int sum = 0;
+    //     for(int num: nums){
+    //         sum += num;
+    //     }
+
+    //     if(sum % 2 != 0) return false;
+    //     int[][] dp = new int[n][(sum/2) + 1];
+    //     for(int[] arr: dp){
+    //         Arrays.fill(arr, -1);
+    //     }
+
+    //     return solve(n-1, 0, nums, sum / 2, dp);
+    // }
+
+    // Tabulation
+    // TC: O(N*k), SC: O(N*k) where (k is sum / 2)
+    public boolean canPartition(int[] nums){
+        int n = nums.length;
+
+        int sum = 0;
+        for(int num: nums) sum += num;
+
+        if(sum % 2 == 1) return false;
+        int k = sum / 2;
+
+        int[][] dp = new int[n][k + 1];
+
+        // Base Case
+        for(int i=0; i<=k; i++){
+            if(i == 0){
+                dp[0][i] = 1;
+                continue;
+            }
+
+            if(nums[0] == k) dp[0][i] = 1;
+            else dp[0][i] = 0;
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=k; j++){
+                int notTake = dp[i-1][j];
+                int take = 0;
+
+                if(nums[i] <= j){
+                    take = dp[i-1][j - nums[i]];
+                }
+
+                if(take == 1 || notTake == 1) dp[i][j] = 1;
+                else dp[i][j] = 0;
             }
         }
+
+        return dp[n-1][k] == 1 ? true : false;
     }
-   
-    return dp[n][sum];
 }
