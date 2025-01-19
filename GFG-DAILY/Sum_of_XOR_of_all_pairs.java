d17990037772f9e936897a713d8c387a5032f476
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-19 16:39:25 +0530

    Added Sum_of_XOR_of_all_pairs.java

---
+++ GFG-DAILY/Sum_of_XOR_of_all_pairs.java
@@ -0,0 +1,35 @@
+//User function Template for Java
+
+class Solution{
+    
+    public boolean checkBit(int N, int i){
+        if((N & (1 << i)) != 0) return true;
+        return false;
+    }
+    
+    public long sumXOR(int arr[], int n) {
+        
+        long ans = 0;
+        
+        for(int i=31; i>=0; i--){
+            
+            long count = 0;
+            for(int j=0; j<n; j++){
+                if(!checkBit(arr[j], i)){
+                    count++;
+                }
+            }
+            
+            long ones = n - count;
+            long temp = (ones * count) * (1 << i);
+            
+            ans += temp;
+        }
+        
+        return ans;
+    }
+    
+    
+}
+
+
