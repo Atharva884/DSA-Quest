85c7f66b95cc117e250056bd3f16211b00a0a449
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-24 17:00:28 +0530

    Added 191-Number_of_1_Bits.java

--- LEETCODE-Questions/191-Number_of_1_Bits.java
+++ LEETCODE-Questions/191-Number_of_1_Bits.java
@@ -1,14 +1,29 @@
 class Solution {
-    // TC: O(N), SC: O(1)
-    public int hammingWeight(int n) {
-        int ans = 0;
+    // Not Efficent
+    // TC: O(32), SC: O(1)
+    // public int hammingWeight(int n) {
+    //     int ans = 0;
 
-        for(int i=31; i>=0; i--){
-            if((n & (1 << i)) != 0){
-                ans++;
-            }
+    //     for(int i=31; i>=0; i--){
+    //         if((n & (1 << i)) != 0){
+    //             ans++;
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+
+    // Efficent (Using bit manipulation)
+    // TC: O(1), SC: O(1)
+    public int hammingWeight(int n){
+        int cnt = 0;
+
+        while(n > 0){
+            cnt += (n & 1);      // It checks for even or odd
+
+            n = n >> 1;          // Dividing by 2
         }
 
-        return ans;
+        return cnt;
     }
 }
