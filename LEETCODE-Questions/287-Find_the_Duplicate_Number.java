e6fe5785dd8fece2934fe4a2fc8ae4b156e8dd78
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-24 17:50:41 +0530

    Added 287-Find_the_Duplicate_Number.java

---
+++ LEETCODE-Questions/287-Find_the_Duplicate_Number.java
@@ -0,0 +1,45 @@
+class Solution {
+    // Brute Force Approach
+    // TC: O(N), SC: O(N)
+    // public int findDuplicate(int[] nums) {
+    //     int n = nums.length;
+
+    //     HashSet<Integer> hs = new HashSet<>();
+
+    //     for(int num: nums){
+    //         if(hs.contains(num)){
+    //             return num;
+    //         }
+
+    //         hs.add(num);
+    //     }
+
+    //     return -1;
+    // }
+
+    public int findDuplicate(int[] nums){
+        int n = nums.length;
+
+        int s = nums[0];
+        int f = nums[0];
+
+        while(true){
+            s = nums[s];
+            f = nums[nums[f]];
+
+            if(s == f){
+                break;
+            }
+        }
+
+
+        s = nums[0];
+
+        while(s != f){
+            s = nums[s];
+            f = nums[f];
+        }
+
+        return s;
+    }
+}
