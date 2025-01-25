6edd828ddf151be99901147c8c972248d75e6558
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-25 19:24:15 +0530

    Added 268-Missing_Number.java

--- LEETCODE-Questions/268-Missing_Number.java
+++ LEETCODE-Questions/268-Missing_Number.java
@@ -1,19 +1,43 @@
 class Solution {
     // Brute Approach
     // TC: O(N), SC: O(N)
+
+    // public int missingNumber(int[] nums) {
+    //     int n = nums.length;
+
+    //     int[] freq = new int[n+1];
+
+    //     for(int num: nums){
+    //         freq[num]++;
+    //     }
+
+    //     for(int i=0; i<n+1; i++){
+    //         if(freq[i] == 0) return i;
+    //     }
+
+    //     return -1;
+    // }
+
+    // Optimal Approach
+    // TC: O(N), SC: O(1)
+
+    // Approach
+    // As we know, there is only 1 element which is missing from the nums array and if 
+    // we do xor with the range (0 -> n) & all the elements of the num, there will be only 
+    // 1 element which will survive as it was missing no in the nums
     public int missingNumber(int[] nums) {
         int n = nums.length;
 
-        int[] freq = new int[n+1];
+        int xor = 0;
 
         for(int num: nums){
-            freq[num]++;
+            xor ^= num;
         }
 
-        for(int i=0; i<n+1; i++){
-            if(freq[i] == 0) return i;
+        for(int i=0; i<=n; i++){
+            xor ^= i;
         }
 
-        return -1;
+        return xor;
     }
 }
