b1f6c3290649a5a86a521312569d9d8d353add2d
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-01-25 23:03:17 +0530

    Added 1470-Shuffle_the_Array.java

---
+++ LEETCODE-Questions/1470-Shuffle_the_Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int len = nums.length;
+
+        int[] ans = new int[len];
+
+        int i = 0;
+        int j = n;
+        int k = 0;
+
+        while(i < n && j < len){
+            ans[k] = nums[i];
+            k++;
+            ans[k] = nums[j];
+            k++;
+
+            i++;
+            j++;
+        }
+
+        return ans;
+    }
+}
