46c6def8b3c26a08a0a2df0f69ecddb31cb0c23a
Atharva Jadhav <93319665+Atharva884@users.noreply.github.com>
2025-02-08 16:46:22 +0530

    Create bubble_sort.java

---
+++ LEETCODE-Questions/bubble_sort.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+     public static void swap(int i, int j, int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    swap(j, j+1, arr);
+                }
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
