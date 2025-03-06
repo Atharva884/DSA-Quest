dad6f589ef040e41bd72f5e5fa2bfebd474b5233
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-03-06 13:35:22 +0530

    Added K-th_element_of_two_Arrays.java

--- GFG-DAILY/K-th_element_of_two_Arrays.java
+++ GFG-DAILY/K-th_element_of_two_Arrays.java
@@ -1,142 +1,41 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int k = Integer.parseInt(br.readLine().trim());
-
-            String[] line1 = br.readLine().trim().split(" ");
-            int[] arr1 = new int[line1.length];
-            for (int i = 0; i < line1.length; i++) {
-                arr1[i] = Integer.parseInt(line1[i]);
-            }
-
-            String[] line2 = br.readLine().trim().split(" ");
-            int[] arr2 = new int[line2.length];
-            for (int i = 0; i < line2.length; i++) {
-                arr2[i] = Integer.parseInt(line2[i]);
-            }
-
-            Solution ob = new Solution();
-            System.out.println(ob.kthElement(k, arr1, arr2));
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
-    // Brute Force Approach
-    // TC: O(N+M), SC: O(N+M)
-    // public static int[] mergeTwoSortedArr(int[] arr1, int[] arr2){
-    //     int n = arr1.length;
-    //     int m = arr2.length;
+    // TC: O(log(Math.min(a, b))), SC: O(1)
+    public int kthElement(int a[], int b[], int k) {
+        int n1 = a.length;
+        int n2 = b.length;
         
-    //     int[] sortArr = new int[n+m];
+        if(n1 > n2) return kthElement(b, a, k);
         
-    //     int i = 0;
-    //     int j = 0;
-    //     int k = 0;
+        int lo = Math.max(0, k - n2);
+        int hi = Math.min(n1, k);
         
-    //     while(i < n && j < m){
-    //         if(arr1[i] < arr2[j]){
-    //             sortArr[k++] = arr1[i];
-    //             i++;
-    //         }else{
-    //             sortArr[k++] = arr2[j];
-    //             j++;
-    //         }
-    //     }
+        int left = k;
         
-    //     // If there are element left in arr1
-    //     while(i < n){
-    //         sortArr[k++] = arr1[i];
-    //         i++;
-    //     }
-        
-    //     while(j < m){
-    //         sortArr[k++] = arr2[j];
-    //         j++;
-    //     }
-        
-    //     return sortArr;
-    // }
-    
-    // public long kthElement(int k, int arr1[], int arr2[]) {
-    //     int[] ans = mergeTwoSortedArr(arr1, arr2);
-    //     int n = ans.length;
-        
-    //     if(k > n){
-    //         return (long) -1;
-    //     }
-        
-    //     long kth = ans[k-1];
-        
-    //     return kth;
-    // }
-    
-    // Optimal Approach -> {In terms of space}
-    // TC: Worst Case: O(N+M), SC: O(1)
-    public static long mergeTwoSortedArr(int[] arr1, int[] arr2, int kIdx){
-        int n = arr1.length;
-        int m = arr2.length;
-    
-        int i = 0;
-        int j = 0;
-        int k = 1; // K starts from 1
-    
-        while (i < n && j < m) {
-            if (arr1[i] < arr2[j]) {
-                
-                if (k == kIdx) {
-                    return arr1[i];
-                }
-                k++;
-                i++;
-            } else {
-                
-                if (k == kIdx) {
-                    return arr2[j];
-                }
-                k++;
-                j++;
+        while(lo <= hi){
+            int mid1 = lo + ((hi - lo) >> 1);
+            int mid2 = left - mid1;
+            
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            
+            if(mid1 < n1) r1 = a[mid1];
+            if(mid2 < n2) r2 = b[mid2];
+            if(mid1 - 1 >= 0) l1 = a[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = b[mid2 - 1];
+            
+            if(l1 <= r2 && l2 <= r1){
+                return Math.max(l1, l2);
+            }else if(l1 > r2){
+                hi = mid1 - 1;
+            }else{
+                lo = mid1 + 1;
             }
         }
-    
-        // If there are elements left in arr1
-        while (i < n) {
-            if (k == kIdx) {
-                return arr1[i];
-            }
-            k++;
-            i++;
-        }
-    
-        // If there are elements left in arr2
-        while (j < m) {
-            if (k == kIdx) {
-                return arr2[j];
-            }
-            k++;
-            j++;
-        }
-    
-        return -1;
-    }
-    
-    public long kthElement(int k, int arr1[], int arr2[]) {
-        long ans = mergeTwoSortedArr(arr1, arr2, k);
         
-        return ans;
+        return 0;
     }
 }
