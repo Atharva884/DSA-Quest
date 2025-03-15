75640dd292552626bf73a8c20af1564a6c30bc5e
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-03-15 21:00:48 +0530

    Added Kth_Smallest.java

---
+++ GFG-DAILY/Kth_Smallest.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        int n = arr.length;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int num: arr){
+            pq.add(num);
+            
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        
+        return pq.peek();
+    }
+}
