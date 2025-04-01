b5bc5e1ceabb20579af976f1866349eac5e9a56f
Atharva Jadhav <atharvalolzzz96@gmail.com>
2025-04-01 13:04:33 +0530

    Added 355-Design_Twitter.java

--- LEETCODE-Questions/355-Design_Twitter.java
+++ LEETCODE-Questions/355-Design_Twitter.java
@@ -1,115 +1,69 @@
-class Pair {
-    int tweetId;
-    int time;
+class Pair implements Comparable<Pair> {
+    int id;
+    int timestamp;
 
-    public Pair(int _tweetId, int _time) {
-        this.tweetId = _tweetId;
-        this.time = _time;
+    public Pair(int _id, int _timestamp) {
+        this.id = _id;
+        this.timestamp = _timestamp;
+    }
+
+    @Override
+    public int compareTo(Pair o) {
+        return o.timestamp - this.timestamp; 
     }
 }
 
 class Twitter {
-    HashMap<Integer, List<Pair>> userMap;
-    HashMap<Integer, List<Integer>> followMap;
-    int time;
+    HashMap<Integer, List<Pair>> map;  
+    HashMap<Integer, List<Integer>> users;  
+    int timestamp;
 
     public Twitter() {
-        userMap = new HashMap<>();
-        followMap = new HashMap<>();
-        time = 0;
+        map = new HashMap<>();
+        users = new HashMap<>();
+        timestamp = 0;
     }
 
     public void postTweet(int userId, int tweetId) {
-        List<Pair> list = userMap.getOrDefault(userId, new ArrayList<>());
-        time++;
-        list.add(new Pair(tweetId, time));
-        userMap.put(userId, list);
+        List<Pair> temp = map.getOrDefault(userId, new ArrayList<>());
+        temp.add(new Pair(tweetId, timestamp++));
+        map.put(userId, temp);
     }
 
     public List<Integer> getNewsFeed(int userId) {
-        // List<Integer> followers = followMap.getOrDefault(userId, new ArrayList<>());
-        // ArrayList<Pair> arr = new ArrayList<>();
-
-        // for (int followerId : followers) {
-        // if (userMap.containsKey(followerId)) {
-        // arr.addAll(userMap.get(followerId));
-        // }
-        // }
-
-        // if (userMap.containsKey(userId)) {
-        // arr.addAll(userMap.get(userId));
-        // }
-
-        // arr.sort((a, b) -> b.time - a.time);
-
-        List<Integer> followers = followMap.getOrDefault(userId, new ArrayList<>());
-        ArrayList<Pair> arr = new ArrayList<>();
-
-        if (followers.size() != 0) {
-            for (int i = 0; i < followers.size(); i++) {
-                int uId = followers.get(i);
-                if (userMap.containsKey(uId)) {
-                    List<Pair> tweets = userMap.get(uId);
-                    for (int j = 0; j < tweets.size(); j++) {
-                        arr.add(tweets.get(j));
-                    }
-                }
-            }
-        }
-
-        List<Pair> tweets = userMap.get(userId);
-        if (tweets != null && tweets.size() != 0) {
-            for (int j = 0; j < tweets.size(); j++) {
-                arr.add(tweets.get(j));
-            }
-        }
-
-        Comparator comp = new Comparator<Pair>() {
-            public int compare(Pair a, Pair b) {
-                // Descending order based on time
-                return b.time - a.time;
-            }
-        };
-
-        Collections.sort(arr, comp);
-
         List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < arr.size() && i < 10; i++) {
-            ans.add(arr.get(i).tweetId);
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        for (Pair tweet : map.getOrDefault(userId, new ArrayList<>())) {
+            pq.add(tweet);
+        }
+
+        for (int follower : users.getOrDefault(userId, new ArrayList<>())) {
+            for (Pair tweet : map.getOrDefault(follower, new ArrayList<>())) {
+                pq.add(tweet);
+            }
+        }
+
+        int k = 10;
+        while (k-- > 0 && !pq.isEmpty()) {
+            ans.add(pq.poll().id);
         }
 
         return ans;
     }
 
     public void follow(int followerId, int followeeId) {
-        List<Integer> list = followMap.getOrDefault(followerId, new ArrayList<>());
-        if (!list.contains(followeeId)) {
-            list.add(followeeId);
+        List<Integer> temp = users.getOrDefault(followerId, new ArrayList<>());
+        if (!temp.contains(followeeId)) { 
+            temp.add(followeeId);
+            users.put(followerId, temp);
         }
-        followMap.put(followerId, list);
     }
 
     public void unfollow(int followerId, int followeeId) {
-        List<Integer> list = followMap.get(followerId);
-
-        if (list == null || list.isEmpty()) {
-            followMap.remove(followerId);
+        List<Integer> temp = users.getOrDefault(followerId, new ArrayList<>());
+        if(temp.size() == 0) 
             return;
-        }
-
-        if (list == null || list.size() == 0) {
-            return;
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == followeeId) {
-                int temp = list.get(i);
-                list.set(i, list.get(list.size() - 1));
-                list.set(list.size() - 1, temp);
-                break;
-            }
-        }
-
-        list.remove(list.size() - 1);
+        temp.remove(Integer.valueOf(followeeId));
     }
 }
