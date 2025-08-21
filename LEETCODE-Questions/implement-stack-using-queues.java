a58b87f8afd62ed0cc1fd8afb7772b270c5661c7
Atharva Jadhav <93319665+Atharva884@users.noreply.github.com>
2025-08-22 00:46:53 +0530

    Create implement-stack-using-queues.java

---
+++ LEETCODE-Questions/implement-stack-using-queues.java
@@ -0,0 +1,66 @@
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    // TC: O(1), SC: O(1)
+    public void push(int x) {
+        q1.add(x);
+    }
+    
+    // TC: O(N + N) ~ O(2N), SC: O(N)
+    public int pop() {
+        int n = q1.size();
+
+        int i = 0;
+        while(!q1.isEmpty() && i < n-1){
+            q2.add(q1.poll());
+            i++;
+        }
+
+        int ans = q1.poll();
+
+        while(!q2.isEmpty()){
+            q1.add(q2.poll());
+        }
+
+        return ans;
+    }
+    
+    // TC: O(N + N) ~ O(2N), SC: O(N)
+    public int top() {
+        int n = q1.size();
+        
+        int i = 0;
+        while(!q1.isEmpty() && i < n-1){
+            q2.add(q1.poll());
+            i++;
+        }
+
+        int ans = q1.poll();
+
+        while(!q2.isEmpty()){
+            q1.add(q2.poll());
+        }
+        q1.add(ans);
+
+        return ans;   
+    }
+    
+    // TC: O(1), SC: O(1)
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
