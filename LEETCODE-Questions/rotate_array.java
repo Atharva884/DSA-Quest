d93c7da99c3b338537a1847c7d7c335682b87396
Atharva Jadhav <93319665+Atharva884@users.noreply.github.com>
2025-08-24 11:14:23 +0530

    Update rotate_array.java

--- LEETCODE-Questions/rotate_array.java
+++ LEETCODE-Questions/rotate_array.java
@@ -21,10 +21,42 @@ class Solution {
             nums[i] = ans[i];
         }
     }
-    
+
+    public static void swap(int[] arr, int sp, int ep){
+        int temp = arr[sp];
+        arr[sp] = arr[ep];
+        arr[ep] = temp;
+    }
+
+    public void reversePart(int[] nums, int sp, int ep){
+        while(sp < ep){
+            swap(nums, sp, ep);
+            
+            sp++;
+            ep--;
+        }
+    }
+
+    public void optimal(int[] nums, int k){
+        int n = nums.length;
+
+        int shift = k;
+        if(k > n){
+            shift = k % n;
+        }
+
+        reversePart(nums, 0, n-shift-1);
+        reversePart(nums, n-shift, n-1);   
+        reversePart(nums, 0, n-1);        
+    }
+
     public void rotate(int[] nums, int k) {
         // Brute Approach
         // TC: O(N), SC: O(N)
-        return brute(nums, k);
+        // brute(nums, k);
+
+        // Optimal Approach
+        // TC: O(N), SC: O(1)
+        optimal(nums, k);
     }
 }
