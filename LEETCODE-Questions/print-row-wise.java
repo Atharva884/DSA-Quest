f36f482d9bbe612d63bcebbc526dc2f9a7dda060
Atharva Jadhav <93319665+Atharva884@users.noreply.github.com>
2025-09-18 09:08:43 +0530

    Create print-row-wise.java

---
+++ LEETCODE-Questions/print-row-wise.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int[][] mat = new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                mat[i][j] = scn.nextInt();
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
