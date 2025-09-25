3a6e89a6d949e9af891c6a93e28a85ced6b475b4
Atharva Jadhav <93319665+Atharva884@users.noreply.github.com>
2025-09-25 15:29:07 +0530

    Update trapping-rain-water.java

--- LEETCODE-Questions/trapping-rain-water.java
+++ LEETCODE-Questions/trapping-rain-water.java
@@ -9,7 +9,7 @@ class Solution {
 
         return pfMax;
     }
-
+    
     public int[] suffixMax(int[] arr, int n){
         int[] sfMax = new int[n];
         sfMax[n-1] = arr[n-1];
@@ -20,23 +20,35 @@ class Solution {
 
         return sfMax;
     }
+    
 
-    // TC: O(N), SC: O(N)
-    public int trap(int[] height) {
-        int n = height.length;
-        int ans = 0;
-
+    public int optimal(int[] height, int n){
         int[] pfMax = prefixMax(height, n);
         int[] sfMax = suffixMax(height, n);
 
+        int sum = 0;
         for(int i=1; i<n-1; i++){
             int lb = pfMax[i-1];
             int rb = sfMax[i+1];
-            int yb = Math.min(lb, rb) - height[i];
+            int contri = Math.min(lb, rb) - height[i];
 
-            if(yb >= 1) ans += yb;
+            if(contri >= 0) sum += contri;
         }
 
-        return ans;
+        return sum;
+    }
+
+    public int trap(int[] height) {
+        int n = height.length;
+
+        // Brute Approach
+        // For each index, run the loop from (0, i-1) and find the left max blockade and also
+        // loop from (i+1, n-1) and find the right max blockade and then compute how much water
+        // can be stored at index i.
+        // TC: O(N^2), SC: O(1)
+
+        // Optimal Approach
+        // TC: O(N+N+N) ~ O(N), SC: O(N)
+        return optimal(height, n);
     }
 }
