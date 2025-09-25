b038d5068599e07298ebbc5814dd9cce7604dc82
Atharva Jadhav <93319665+Atharva884@users.noreply.github.com>
2025-09-25 15:08:01 +0530

    Update largest-number-at-least-twice-of-others.java

--- LEETCODE-Questions/largest-number-at-least-twice-of-others.java
+++ LEETCODE-Questions/largest-number-at-least-twice-of-others.java
@@ -1,22 +1,56 @@
 class Solution {
-    // TC: O(N + N) ~ O(N), SC: O(1) 
+    
+    public int brute(int[] arr, int n){
+        int max = -1;
+        int idx = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                idx = i;
+            }
+        }
+
+        for(int num: arr){
+            if(num == max) continue;
+
+            if(num * 2 > max){
+                return -1;
+            }
+        }
+
+        return idx;
+    }
+
+    public int optimal(int[] nums, int n){
+        int max1 = -1;
+        int max2 = -1;
+        int idx = 0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] > max1){
+                max2 = max1;
+                max1 = nums[i];
+                idx = i;
+            }else if(nums[i] < max1 && nums[i] > max2){
+                max2 = nums[i];
+            }
+        }
+
+        // System.out.println(max1 + " " + max2);
+        // System.out.println(idx);
+        return max2 * 2 > max1 ? -1 : idx;
+    }
+
     public int dominantIndex(int[] nums) {
         int n = nums.length;
 
-        int max = 0;
-        int idx = -1;
-        for(int i=0; i<n; i++){
-            if(nums[i] > max){
-                max = nums[i];
-                idx = i;
-            }   
-        }   
+        // Optimal Approach -> But two passes
+        // TC: O(N) + O(N) ~ O(N), SC: O(1)
+        // return brute(nums, n);
 
-        for(int i=0; i<n; i++){
-            if(nums[i] == max) continue;
-            if(max < 2 * nums[i]) return -1;
-        }        
-
-        return idx;
+        // Optimal Approach -> Only Single Pass
+        // TC: O(N) + O(N) ~ O(N), SC: O(1)
+        return optimal(nums, n);
     }
 }
